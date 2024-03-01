11aafb1c6113ec290a920d2c323bd2acc3a2bd6d
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 <picard.boris@gmail.com>
2024-03-01 15:18:19 +0100

    filtre de catégories terminé

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,50 @@
-import './assets/styles/body.css'
+import "./assets/styles/body.css";
 import { AddTask } from "./components/form/AddTask";
 import { useState } from "react";
 
 function App() {
-
   const [tasks, setTasks] = useState([]);
-  const [filteredTask, setFilteredTask] = useState("All")
+  const [filteredTask, setFilteredTask] = useState("All");
 
   const addTask = (task) => {
     const newTask = { id: Date.now(), text: task, isDone: false };
-    setTasks([...tasks, newTask])
-  }
+    setTasks([...tasks, newTask]);
+  };
 
   const deleteTasks = (taskId) => {
-    setTasks(tasks.filter(task => task.id !== taskId))
-  }
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
 
   const handleClick = (id) => {
-    const updatedTasks = tasks.map(task => {
+    const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
         if (task.isDone === true) {
-          return { ...task, isDone: false }
+          return { ...task, isDone: false };
         }
-        return { ...task, isDone: true }
+        return { ...task, isDone: true };
       }
       return task;
-    })
-    setTasks(updatedTasks)
-  }
+    });
+    setTasks(updatedTasks);
+  };
 
   const getValue = (e) => {
-    setFilteredTask(e.target.value)
-  }
-  
-  const filterTask = () => {
-    const newFilteredTasks = setTasks([...tasks])
-    console.log(tasks);
+    setFilteredTask(e.target.value);
+  };
+
+  const filterTask = (tasks) => {
     switch (filteredTask) {
       case "All":
-        return newFilteredTasks
+        return tasks;
       case "Active":
-        return newFilteredTasks.filter((task) => task.isDone === false)
+        return tasks.filter((task) => task.isDone === false);
       case "Completed":
-        return newFilteredTasks.filter((task) => task.isDone === true)
+        return tasks.filter((task) => task.isDone === true);
       default:
         break;
     }
-  }
-  
+  };
+
   return (
     <section>
       <div className="container my-5">
@@ -65,53 +62,56 @@ function App() {
               <button
                 className="btn btn-sm btn-light mx-3 fw-bold rounded-3 p-2"
                 onClick={getValue}
-                value="All">
+                value="All"
+              >
                 Toutes les tâches
               </button>
               <button
                 className="btn btn-sm btn-light mx-3 fw-bold rounded-3 p-2"
                 onClick={getValue}
-                value="Active">
+                value="Active"
+              >
                 Tâches actives(non complétées)
               </button>
               <button
                 className="btn btn-sm btn-light mx-3 fw-bold rounded-3 p-2"
                 onClick={getValue}
-                value="Completed">
+                value="Completed"
+              >
                 Tâches complétées
               </button>
             </div>
           </div>
           <div className="row justify-content-center">
             <div className="col-8 my-5">
-              {tasks.map((task) =>
+              {filterTask(tasks).map((task) => (
                 <div
-                  className={`hstack mb-3 card bg-light ${task.isDone ? 'text-decoration-line-through' : null}`}
+                  className={`hstack mb-3 card bg-light ${
+                    task.isDone ? "text-decoration-line-through" : null
+                  }`}
                   id={task.id}
                   key={task.id}
-                  onChange={filterTask}
                   onClick={() => handleClick(task.id)}
                 >
-                  <div className="card-body fw-bold">
-                    {task.text}
-                  </div>
+                  <div className="card-body fw-bold">{task.text}</div>
                   <button
                     className="mx-2 btn btn-sm btn-danger"
                     id={task.id}
                     onClick={(e) => {
                       e.stopPropagation();
-                      deleteTasks(task.id)
-                    }}>
+                      deleteTasks(task.id);
+                    }}
+                  >
                     Supprimer la tâche
                   </button>
                 </div>
-              )}
+              ))}
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/form/AddTask.jsx
+++ src/components/form/AddTask.jsx
@@ -1,28 +1,29 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export function AddTask({onAddTask}) {
+export function AddTask({ onAddTask }) {
+  const [list, setList] = useState("");
 
-    const [list, setList] = useState("")
+  const handleChange = (e) => {
+    setList(e.target.value);
+  };
 
-    const handleChange = (e) => {
-        setList(e.target.value)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (list) {
+      onAddTask(list);
+      setList("");
     }
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (list) {
-            onAddTask(list)
-            setList("")
-        }
-    }
-
-    return <form onSubmit={handleSubmit}>
-        <input
-            className="form-control"
-            type="text"
-            value={list}
-            placeholder="Ajouter une tâche"
-            onChange={handleChange}
-        />
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="form-control"
+        type="text"
+        value={list}
+        placeholder="Ajouter une tâche"
+        onChange={handleChange}
+      />
     </form>
+  );
 }
