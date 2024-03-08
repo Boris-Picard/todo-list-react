d04b62d767e41c9ce8f511923ccb1ca56fa87e3e
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 <picard.boris@gmail.com>
2024-03-08 11:33:45 +0100

    installation tailwind + nextui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./assets/styles/body.css";
 import { AddTask } from "./components/form/AddTask";
 import { useEffect, useState } from "react";
 import ModifyTask from "./components/form/ModifyTask";
+import { NextUIProvider } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -91,98 +93,100 @@ function App() {
     return setTasks(modifyTask);
   };
   return (
-    <section>
-      <div className="container my-5">
-        <div className="row">
-          <div className="col-12 text-center py-5">
-            <h1 className="text-white">Ma Todo List</h1>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col-6">
-              <AddTask onAddTask={addTask} />
+    <NextUIProvider>
+      <section>
+        <div className="container my-5">
+          <div className="row">
+            <div className="col-12 text-center py-5">
+              <h1 className="text-white">Ma Todo List</h1>
             </div>
-          </div>
-          <div className="row text-center mt-5">
-            <div className="col-12">
-              <button
-                className={`btn btn-sm btn-light mx-3 fw-bold rounded-3 p-2 ${isFilterActive(
-                  "All"
-                )}`}
-                onClick={getValue}
-                value="All"
-              >
-                Toutes les tâches
-              </button>
-              <button
-                className={`btn btn-sm btn-light mx-3 fw-bold rounded-3 p-2 ${isFilterActive(
-                  "Active"
-                )}`}
-                onClick={getValue}
-                value="Active"
-              >
-                Tâches actives(non complétées)
-              </button>
-              <button
-                className={`btn btn-sm btn-light mx-3 fw-bold rounded-3 p-2 ${isFilterActive(
-                  "Completed"
-                )}`}
-                onClick={getValue}
-                value="Completed"
-              >
-                Tâches complétées
-              </button>
+            <div className="row justify-content-center">
+              <div className="col-6">
+                <AddTask onAddTask={addTask} />
+              </div>
             </div>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col-8 my-5">
-              {filterTask(tasks).map((task) => (
-                <div
-                  className={`hstack mb-3 card bg-light ${
-                    task.isDone ? "text-decoration-line-through" : null
-                  }`}
-                  id={task.id}
-                  key={task.id}
-                  onClick={(e) => handleClick(task.id, e)}
+            <div className="row text-center mt-5">
+              <div className="col-12">
+                <button
+                  className={`btn btn-sm btn-light mx-3 fw-bold rounded-3 p-2 ${isFilterActive(
+                    "All"
+                  )}`}
+                  onClick={getValue}
+                  value="All"
                 >
-                  <div className="card-body fw-bold">{task.text}</div>
-                  {editingTaskId === task.id ? (
-                    <ModifyTask
-                      key={task.id}
-                      id={task.id}
-                      onModifyTodo={(newValue) => {
-                        modifyTodo(task.id, newValue);
-                        handFinishEditing();
-                      }}
-                    />
-                  ) : (
+                  Toutes les tâches
+                </button>
+                <button
+                  className={`btn btn-sm btn-light mx-3 fw-bold rounded-3 p-2 ${isFilterActive(
+                    "Active"
+                  )}`}
+                  onClick={getValue}
+                  value="Active"
+                >
+                  Tâches actives(non complétées)
+                </button>
+                <button
+                  className={`btn btn-sm btn-light mx-3 fw-bold rounded-3 p-2 ${isFilterActive(
+                    "Completed"
+                  )}`}
+                  onClick={getValue}
+                  value="Completed"
+                >
+                  Tâches complétées
+                </button>
+              </div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="col-8 my-5">
+                {filterTask(tasks).map((task) => (
+                  <div
+                    className={`hstack mb-3 card bg-light ${
+                      task.isDone ? "text-decoration-line-through" : null
+                    }`}
+                    id={task.id}
+                    key={task.id}
+                    onClick={(e) => handleClick(task.id, e)}
+                  >
+                    <div className="card-body fw-bold">{task.text}</div>
+                    {editingTaskId === task.id ? (
+                      <ModifyTask
+                        key={task.id}
+                        id={task.id}
+                        onModifyTodo={(newValue) => {
+                          modifyTodo(task.id, newValue);
+                          handFinishEditing();
+                        }}
+                      />
+                    ) : (
+                      <button
+                        className="btn btn-sm btn-dark"
+                        id={task.id}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleModify(task.id);
+                        }}
+                      >
+                        <i className="bi bi-pencil-square"></i>
+                      </button>
+                    )}
                     <button
-                      className="btn btn-sm btn-dark"
+                      className="mx-2 btn btn-sm btn-danger"
                       id={task.id}
                       onClick={(e) => {
                         e.stopPropagation();
-                        handleModify(task.id);
+                        deleteTasks(task.id);
                       }}
                     >
-                      <i className="bi bi-pencil-square"></i>
+                      <i className="bi bi-trash-fill mx-2"></i>
                     </button>
-                  )}
-                  <button
-                    className="mx-2 btn btn-sm btn-danger"
-                    id={task.id}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      deleteTasks(task.id);
-                    }}
-                  >
-                    <i className="bi bi-trash-fill mx-2"></i>
-                  </button>
-                </div>
-              ))}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </NextUIProvider>
   );
 }
 
